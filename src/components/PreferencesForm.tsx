0095ca8a7ce03046abf3d607e06e390acc3d5b13
Sarah Pickrahn <spickrahn@gmail.com>
2024-02-01 11:35:23 -0800

    wip stepper

--- src/components/PreferencesForm.tsx
+++ src/components/PreferencesForm.tsx
@@ -1,8 +1,10 @@
-import { Button } from "antd";
 import { IntakeQuestion } from "./IntakeQuestion";
 import { getInitialValues, intakeQuestions } from "@/utils/intakeQuestions";
 import { PlanConstraints } from "@/utils/types.d";
 import { Form, Formik } from "formik";
+import MobileStepper from "@mui/material/MobileStepper";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 
 const initialValues = getInitialValues();
 
@@ -11,17 +13,41 @@ export interface PreferencesFormProps {
 }
 
 export const PreferencesForm = ({ onFormSubmit }: PreferencesFormProps) => {
+  const [step, setStep] = useState(0);
+
+  const handleNext = () => {
+    setStep((prevStep) => prevStep + 1);
+  };
+
+  const handleBack = () => {
+    setStep((prevStep) => prevStep - 1);
+  };
+
   return (
     <Formik initialValues={initialValues} onSubmit={onFormSubmit}>
       <Form>
-        {intakeQuestions.map((question) => (
-          <div key={question.name} className="gap-2">
-            <IntakeQuestion question={question} />
-          </div>
-        ))}
-        <Button className="mt-3" type="primary" htmlType="submit">
-          generate
-        </Button>
+        <MobileStepper
+          variant="progress"
+          steps={intakeQuestions.length}
+          activeStep={step}
+          position="static"
+          nextButton={
+            <Button
+              size="small"
+              onClick={handleNext}
+              disabled={step === intakeQuestions.length - 1}
+            >
+              Next
+            </Button>
+          }
+          backButton={
+            <Button size="small" onClick={handleBack} disabled={step === 0}>
+              Back
+            </Button>
+          }
+        />
+        <IntakeQuestion question={intakeQuestions[step]} />
+        <Button className="mt-3 hover:bg-slate-500">generate</Button>
       </Form>
     </Formik>
   );
